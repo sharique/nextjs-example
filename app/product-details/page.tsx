7ea38f3c324a64d6d64551e43415e65b12b04fa2
Sharique A Farooqui <safknw@gmail.com>
2024-12-17 12:38:29 +0100

    Implemnted pages.

--- app/product-details/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function ProductDetailPage() {
-  return <h1>Products details</h1>;
-}
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,11 @@
-import ProductList from "../ProductList";
-import { products } from "../produt-data";
+import ProductsList from "../ProductsList";
+import { products } from "../product-data";
+
 export default function ProductsPage() {
   return (
-    <>
-      <h1>Products list</h1>
-      <ProductList products={products} />
-    </>
+    <div className="container mx-auto p-8">
+      <h1 className="text-4xl font-bold mb-8">Products</h1>
+      <ProductsList products={products} />
+    </div>
   );
 }
