bd9e14508600d9d9aa3eea277865154c10d48952
Sharique A Farooqui <safknw@gmail.com>
2025-01-10 22:31:51 +0100

    Adding tests.

---
+++ app/page.test.tsx
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Home from "../app/page";
+
+test("Page", () => {
+  render(<Home />);
+  expect(
+    screen.getByRole("heading", {
+      level: 1,
+      name: "Welcome to Example ecommerce app.",
+    }),
+  ).toBeDefined();
+});
